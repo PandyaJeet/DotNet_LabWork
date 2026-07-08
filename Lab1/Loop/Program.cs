f17e4b026128858699495bf08890a89a1c8da165
Jeet Pandya <jeetpandya2006@gmail.com>
2026-07-08 06:03:20 +0000

    UserInput for Looping [9-7-26]

---
+++ Lab1/Loop/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.Versioning;
+
+class Hello
+{
+    static void loop()
+    {
+        Console.Write("Start Value : ");
+        int start = Convert.ToInt32(Console.ReadLine());
+        Console.Write("End Value : ");
+        int end = Convert.ToInt32(Console.ReadLine());
+        for(int i = start ; i <= end; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+    static void Main()
+    {
+        Hello.loop();
+    }
+}
