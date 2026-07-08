86842bfc7c0a2c093891abc8450292f2059ab465
Jeet Pandya <jeetpandya2006@gmail.com>
2026-07-08 05:56:22 +0000

    Dotnet Setup and Project Initialization

---
+++ Lab1/HelloWorld/Program.cs
@@ -0,0 +1,29 @@
+﻿class Hello
+{
+    static void add()
+    { 
+        Console.Write("No1 : ");
+        int no1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("No2 : ");
+        int no2 = Convert.ToInt32(Console.ReadLine());
+        int sum = no1 + no2;
+        Console.WriteLine("Sum is " + sum);
+    }
+    static void greater()
+    {
+        Console.Write("No1 : ");
+        int no1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("No2 : ");
+        int no2 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("No2 : ");
+        int no3 = Convert.ToInt32(Console.ReadLine());
+        if (no2 > no3 & no2 > no1) Console.WriteLine("No2 i.e. " + no2 + " is greater");
+        else if (no1 > no2 & no1 > no3) Console.WriteLine("No1 i.e. " + no1 + " is greater");
+        else if (no3 > no2 & no3 > no1) Console.WriteLine("No3 i.e. " + no3 + " is greater");
+        else Console.WriteLine("All are equal");
+    }
+    static void Main()
+    {
+        Hello.greater();
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # DotNet_LabWork
+
+Installation 
+    sudo apt-get update && \
+    sudo apt-get install -y dotnet-sdk-10.0
+
+
+Initializing project
+    Navigate to that folder [cd foldername]
+    dotnet new console -o [projectname]
+
+Running
+    cd [projectName]
+    dotnet run
