964cb0949534b6e755117a62e62e94c614e3c3b5
Jeet Pandya <jeetpandya2006@gmail.com>
2026-07-15 04:52:04 +0000

    Assignment 7,8,9

---
+++ Lab2/Assign1/Class/Program.cs
@@ -0,0 +1,21 @@
+﻿class hello
+{
+    public void printdata(int n1, int n2)
+    {
+        Console.Write("Sum of " + n1 + " and " + n2 + " is " + (n1 + n2));
+    }
+}
+class main
+{
+    static void Main()
+    {
+        int n1, n2;
+        Console.Write("Enter a number 1 : ");
+        n1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter a number 2 : ");
+        n2 = Convert.ToInt32(Console.ReadLine());
+        hello hello = new hello();
+        main h = new main();
+        hello.printdata(n1,n2);
+    }
+}
---
+++ Lab2/Assign1/Func_With_Args/Program.cs
@@ -0,0 +1,36 @@
+﻿class Hello
+{
+    int n1,n2;
+    public Hello(Hello g)//Copy Construct
+    {
+        n1 = g.n1;
+        n2 = g.n2;
+    }
+    public Hello(int a,int b)//Param Construct
+    {
+        n1 = a;
+        n2 = b;
+    }
+    public Hello()//Default Construct
+    {
+        n1 = 1;
+        n2 = 2;
+    }
+    public void Print()
+    {
+        Console.WriteLine("N is " + (n1+n2));
+    }
+    
+}
+class Notmain
+{
+    static void Main()
+    {
+        Hello hello = new Hello();
+        Hello k = new Hello(10, 20);
+        k.Print();
+        Hello nothello = new Hello(hello);
+        nothello.Print();
+        hello.Print();
+    }
+}
